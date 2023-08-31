d3a4e4c5c954a20ab27cd94b81237ffde1318671
MirceaDMA <94933605+MirceaDMA@users.noreply.github.com>
2023-08-31 16:46:39 +0000

    added second solution

--- sort_a_string.py
+++ sort_a_string.py
@@ -1,6 +1,11 @@
-def sort_words(words_to_sort):
+# sort words alpabetically: solution 1
+def sort_words_1(words_to_sort):
     words = [word.lower()+word for word in words_to_sort.split()]
     words.sort()
     words = [w[len(w)//2:] for w in words]
-    print(''.join(words))
+    print(' '.join(words))
     return ' '.join(words)
+
+# sort words alpabetically: solution 2
+def sort_words_2(words_to_sort):
+    return " ".join(sorted(words_to_sort.split(), key=str.casefold))
