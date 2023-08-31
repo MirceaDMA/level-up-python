7f5433fa771aad93dc86acdfbc55e1e60b07814f
MirceaDMA <94933605+MirceaDMA@users.noreply.github.com>
2023-08-31 15:23:13 +0000

    added second method of palindrome function (using ReGex module)

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,4 @@
+# my method using string module
 import string
 
 def is_palindrome(phrase):
@@ -7,3 +8,11 @@ def is_palindrome(phrase):
     formatted_phrase = formatted_phrase.replace(" ", "")
     reverse_phrase = formatted_phrase[::-1]
     return formatted_phrase == reverse_phrase
+
+# second method using ReGex
+import re
+
+def is_it_palindrome(phrase):
+    formatted_phrase = "".join(re.findall(r"[a-z]+", phrase.lower()))
+    reversed_phrase = formatted_phrase[::-1]
+    return formatted_phrase == reversed_phrase
