8a10360e09ac019258a6308f3c8f2296ef02452e
MirceaDMA <94933605+MirceaDMA@users.noreply.github.com>
2023-09-01 17:21:18 +0000

    modified:   practice/dictionary.py

--- practice/dictionary.py
+++ practice/dictionary.py
@@ -1,20 +1,21 @@
-import json
-
-def save_dict(dict_to_save, file_path):
-    with open(file_path, "wb", encoding="utf-8") as file:
-        file.write(json.dumps(dict_to_save))
-
-def open_dict(file_path):
-    with open(file_path, "rb") as file:
-        return json.loads(file.read())
-
-# PICKLE
-# import pickle
+# import json
 
 # def save_dict(dict_to_save, file_path):
-#     with open(file_path, "wb") as file:
-#         pickle.dump(dict_to_save, file)
+#     with open(file_path, "wb", encoding="utf-8") as file:
+#         file.write(json.dumps(dict_to_save))
 
-# def load_dict(file_path):
+# def open_dict(file_path):
 #     with open(file_path, "rb") as file:
-#         return pickle.load(file)
+#         return json.loads(file.read())
+
+# PICKLE
+import pickle
+
+def save_dict(dict_to_save, file_path):
+    with open(file_path, "wb") as file:
+        pickle.dump(dict_to_save, file)
+
+def load_dict(file_path):
+    with open(file_path, "rb") as file:
+        return pickle.load(file)
+    
