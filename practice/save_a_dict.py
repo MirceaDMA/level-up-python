f3612cce99fa0fe08b2e7eeffd507b65083dc5be
MirceaDMA <94933605+MirceaDMA@users.noreply.github.com>
2023-09-01 17:01:01 +0000

    JSON solution

---
+++ practice/save_a_dict.py
@@ -0,0 +1,10 @@
+import json
+
+def save_dict(dict_to_save, file_path):
+    with open(file_path, "w", encoding="utf-8") as f:
+        f.write(json.dumps(dict_to_save))
+
+def open_dict(file_path):
+    with open(file_path) as f:
+        new_dictionary = json.loads(f.read())
+    return new_dictionary
