3a8f88f095838910bbb5c0330db1add2913f6f9c
MirceaDMA <94933605+MirceaDMA@users.noreply.github.com>
2023-09-01 17:02:58 +0000

    modified:   practice/save_a_dict.py

--- practice/save_a_dict.py
+++ practice/save_a_dict.py
@@ -5,6 +5,6 @@ def save_dict(dict_to_save, file_path):
         f.write(json.dumps(dict_to_save))
 
 def open_dict(file_path):
-    with open(file_path) as f:
+    with open(file_path, encoding="utf-8") as f:
         new_dictionary = json.loads(f.read())
     return new_dictionary
