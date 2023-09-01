d737dc922df2e14051094370c15b05fce3bc9db5
MirceaDMA <94933605+MirceaDMA@users.noreply.github.com>
2023-09-01 14:54:52 +0000

    final solution

---
+++ practice/list_indices.py
@@ -0,0 +1,12 @@
+def index_all(search_list, item):
+    index_list = []
+    for index, value in enumerate(search_list):
+        if value == item:
+            index_list.append([index])
+        elif isinstance(search_list[index], list):
+            for i in index_all(search_list[index], item):
+                index_list.append([index]+i)
+    return index_list
+    
+# example_list = [[[1, 2, 3], 2, [1, 3]], [1, 2, 3]]
+# search_item = 2
