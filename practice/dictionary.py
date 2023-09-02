ea8e19b686af0c911f87ca3284cd7ade27034912
MirceaDMA <94933605+MirceaDMA@users.noreply.github.com>
2023-09-02 09:08:38 +0000

    new file:   practice/sched_funct.py

--- practice/dictionary.py
+++ practice/dictionary.py
@@ -1,11 +1,11 @@
 import json
 
 def save_dict(dict_to_save, file_path):
-    with open(file_path, "wb", encoding="utf-8") as file:
+    with open(file_path, "w") as file:
         file.write(json.dumps(dict_to_save))
 
 def load_dict(file_path):
-    with open(file_path, "rb") as file:
+    with open(file_path, "r") as file:
         return json.loads(file.read())
 
 # PICKLE
